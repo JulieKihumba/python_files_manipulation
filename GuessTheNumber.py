d4774a5974a87949d171099b4856fd681f304507
JulieKihumba <eng.juliekihumba@gmail.com>
2024-11-28 09:48:26 +0300

    my python file

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -3,14 +3,26 @@ print("I'm thinking of a number between 1 and 10.")
 choice = int(input("what is that number?? "))
 
 import random
-numbers=[1,2,3,4,5,6,7,8,9,10]
-my_choice = random.choice(numbers)
+my_choice = random.randint(1,10)
+guess_limit=3
+score = 0
 
+while guess_limit>1 and choice != my_choice:
+    choice = int(input("Sorry you did not get it right. Guess again.What is that number?? "))
+    guess_limit-=1
 if choice == my_choice:
+    if guess_limit==2:
+        score=7
+    elif guess_limit==1:
+        score=5
+    elif guess_limit==0:
+        score =3
+    
+    print(f"Congratulations!!😁 I chose {my_choice} too.You got it right!!Your score is {score}")
 
-    print(f"I chose {my_choice}. So you got it right!!")
 else:
-    print(f"Sorry you did not get it right, i chose {my_choice} . Please try again next time")
+    score=0
+    print(f"Sorry😞 you did not get it right, i chose {my_choice} . Please try again next time.Your score is {score}")
 
 
 
---
+++ quiz_game.py
@@ -0,0 +1,9 @@
+print("Welcome to the quiz game😁👏 ")
+playing=input("Are you ready to start?(Y/N) ").lower()
+if playing != "y":
+    quit()
+    
+print("Okay!! Let's play😁")
+
+answer=input("What does CPU stand for? ")
+
